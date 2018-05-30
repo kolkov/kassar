1bdf68f279e1efe9c8f5127ff18a33eba7f6b56d
Andy <a.kolkov@gmail.com>
2018-05-30 20:55:31 +0300

    Fix articles

--- ui/src/app/blog/article-list/article-list.component.ts
+++ ui/src/app/blog/article-list/article-list.component.ts
@@ -2,8 +2,9 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {ArticleListItem} from "../article";
 import {ArticleService} from "../article.service";
 import {Observable} from "rxjs/internal/Observable";
-import {map} from "rxjs/operators";
+import {map, tap} from "rxjs/operators";
 import {ActivatedRoute} from "@angular/router";
+import {Category} from "../../catalog/cart-home.service";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,13 +14,16 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ArticleListComponent implements OnInit {
   articles$: Observable<ArticleListItem[]>;
+  category: Category;
 
   constructor(private blogService: ArticleService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     const path = this.route.snapshot.params['id'];
+
     this.articles$ = this.blogService.getList(path).pipe(
+      tap(() => this.category = this.blogService.checkPath(path)),
       map(x => x.items)
     );
   }
