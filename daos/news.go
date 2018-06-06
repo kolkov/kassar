5cfd234f2fef75006061d47cbbce6f9d0366241b
Andy <a.kolkov@gmail.com>
2018-06-07 00:45:34 +0300

    Fix news order

--- daos/news.go
+++ daos/news.go
@@ -20,16 +20,16 @@ func (dao *NewsDAO) Get(rs app.RequestScope, id int) (*models.News, error) {
 	return &newsItem, err
 }
 
-// Count returns the number of the user records in the database.
+// Count returns the number of the news records in the database.
 func (dao *NewsDAO) Count(rs app.RequestScope) (int, error) {
 	var count int
 	err := rs.Tx().Select("COUNT(*)").From("news").Row(&count)
 	return count, err
 }
 
-// Query retrieves the user records with the specified offset and limit from the database.
+// Query retrieves the news records with the specified offset and limit from the database.
 func (dao *NewsDAO) Query(rs app.RequestScope, offset, limit int) ([]models.News, error) {
 	newsFeed := []models.News{}
-	err := rs.Tx().Select().OrderBy("id").Offset(int64(offset)).Limit(int64(limit)).All(&newsFeed)
+	err := rs.Tx().Select().OrderBy("id").Offset(int64(offset)).Limit(int64(limit)).OrderBy("id DESC").All(&newsFeed)
 	return newsFeed, err
 }
